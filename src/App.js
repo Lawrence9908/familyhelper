8edc110b9874c36edd32824ae34c45756dfaaa5b
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2024-05-03 19:05:55 +0200

    added aws-amply authenticator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import Ampliyf from 'aws-amplify'
+import awsConfig from './aws-exports'
+import {AmplifyAuthenticator, AmplifySignOut} from  '@aws-amplify/ui-react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcom to AWS Ampplify Dev</h1>
-    </div>
+    <AmplifyAuthenticator>
+      <div className="App">
+        <h1>Welcom to AWS Ampplify Dev</h1>
+        <AmplifySignOut/>
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
